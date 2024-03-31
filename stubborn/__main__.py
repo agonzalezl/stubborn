04e7bee14a65b930e33464ac49e40846fcb45ec6
agonzalezl <angel.gonzalez.lpz@gmail.com>
2024-03-31 16:23:23 +0200

    Introducing typer (#2)

--- stubborn/__main__.py
+++ stubborn/__main__.py
@@ -1,4 +1,28 @@
-from .main import main
+from typing import Optional
+
+import typer
+from typing_extensions import Annotated
+
+from .main import report_missmatched_versions
+
+
+def main(
+    major: Annotated[
+        Optional[str], typer.Argument(help="Alert on major version missmatch")
+    ] = True,
+    minor: Annotated[
+        Optional[str], typer.Argument(help="Alert on minor version missmatch")
+    ] = True,
+    patch: Annotated[
+        Optional[str], typer.Argument(help="Alert on patch version missmatch")
+    ] = False,
+    path: Annotated[
+        Optional[str],
+        typer.Argument(help="Path to poetry root path", show_default="os.getcwd()"),
+    ] = None,
+):
+    report_missmatched_versions(major, minor, patch, path)
+
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
