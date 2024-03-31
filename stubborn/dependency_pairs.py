e44f3df4951a32d4aafc9b25e484ba0271c93546
agonzalezl <angel.gonzalez.lpz@gmail.com>
2024-03-31 16:39:29 +0200

    Added a bunch of stub libs

--- stubborn/dependency_pairs.py
+++ stubborn/dependency_pairs.py
@@ -1,1 +1,10 @@
-DEPENDENCY_PAIRS = {"botocore": "botocore-stubs", "boto3": "boto3-stubs"}
+DEPENDENCY_PAIRS = {
+    "botocore": "botocore-stubs",
+    "boto3": "boto3-stubs",
+    "aiobotocore": "types-aiobotocore",
+    "requests": "types-requests",
+    "PyYAML": "types-PyYAML",
+    "aioboto3": "types-aioboto3",
+    "aiofiles": "types-aiofiles",
+    "beautifulsoup4": "types-beautifulsoup4",
+}
